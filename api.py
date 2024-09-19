e9cc7ee2109ed8afa2adc879bee5d2d469e51fb6
Saint-Clair <caehevanras@gmail.com>
2024-09-19 14:22:31 -0300

    Inclusão de interface de usuário e outras pequenas alterações

--- api.py
+++ api.py
@@ -1,10 +1,19 @@
 print('Inicializando a estrutura da API...\nImportando as bibliotecas...')
 from fastapi import FastAPI, Query
+from starlette.middleware.cors import CORSMiddleware
 from langchain_huggingface import HuggingFaceEmbeddings
 from gerador_de_respostas import GeradorDeRespostas
+from pydantic import BaseModel
 
 print('Instanciando a api (FastAPI)')
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 print('Criando a função de embeddings')
 funcao_de_embeddings = HuggingFaceEmbeddings(
@@ -15,16 +24,21 @@ funcao_de_embeddings = HuggingFaceEmbeddings(
 
 gerador_de_respostas = GeradorDeRespostas(funcao_de_embeddings=funcao_de_embeddings, url_vector_store='chroma_db_instructor_xl')
 
+
+# Define the Pydantic model for the POST request
+class PerguntaModel(BaseModel):
+    pergunta: str
+
 print('Definindo as rotas')
 @app.post("/get_embedding/")
-async def get_embedding(pergunta: str):
-    resposta = gerador_de_respostas.consultar(pergunta)
+async def get_embedding(dadosRecebidos: PerguntaModel):
+    resposta = gerador_de_respostas.consultar(dadosRecebidos.pergunta)
     return {"resposta": resposta}
 
-# Define a GET route that takes a query parameter 'text'
-@app.get("/get_embedding/")
-async def get_embedding(text: str = Query(..., description="Pergunta/Query a ser buscada no banco de vetores")):
-    resposta = gerador_de_respostas.consultar(text)
-    return {"resposta": resposta}
+# # Define a GET route that takes a query parameter 'text'
+# @app.get("/get_embedding/")
+# async def get_embedding(text: str = Query(..., description="Pergunta/Query a ser buscada no banco de vetores")):
+#     resposta = gerador_de_respostas.consultar(text)
+#     return {"resposta": resposta}
 
 print('API inicializada')
--- gerador_de_respostas.py
+++ gerador_de_respostas.py
@@ -5,6 +5,8 @@ from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
+from time import time
+
 class GeradorDeRespostas:
     '''
     Classe cuja função é realizar consulta em um banco de vetores existente e, por meio de uma API de um LLM
@@ -64,48 +66,69 @@ class GeradorDeRespostas:
 
                 # Placeholder para o input a ser fornecido durante a execução
                 # Será substituído pela pergunta do usuário e o contexto vindo do banco de vetores
-                ("human", "\nCONTEXTO: {contexto} \n\nPERGUNTA: {pergunta}"),
+                ("human", "CONTEXTO: {contexto} \n\nPERGUNTA: {pergunta}"),
             ]
         )
         
-        if verbose: print('--- gerando o runnable base e formatador de saída...')
-        # Esse runnable faz o tratamento da pergunta e do contexto para o prompt
-        self.base_runnable = (
-            {
-                # 'contexto' faz ouso do retrtiever/gerenciador de consulta para obter contexto do banco de vetores
-                # 'gerenciador_de_consulta' faz a busca no banco de vetores com base na pergunta/query
-                # 'formatar_documentos_recuperados' formata os resultados
-                "contexto": self.gerenciador_de_consulta | self.formatar_documentos_recuperados,
-                # 'pergunta' será passada para o retriever/gerenciador de consulta como query
-                "pergunta": RunnablePassthrough(),
-            }
-            # combina o contexto e a pergunta no template do prompt
-            | self.template_do_prompt
-        )
+        # if verbose: print('--- gerando o runnable base e formatador de saída...')
+        # # Esse runnable faz o tratamento da pergunta e do contexto para o prompt
+        # self.base_runnable = (
+        #     {
+        #         # 'contexto' faz ouso do retrtiever/gerenciador de consulta para obter contexto do banco de vetores
+        #         # 'gerenciador_de_consulta' faz a busca no banco de vetores com base na pergunta/query
+        #         # 'formatar_documentos_recuperados' formata os resultados
+        #         "contexto": self.gerenciador_de_consulta | self.formatar_documentos_recuperados,
+        #         # 'pergunta' será passada para o retriever/gerenciador de consulta como query
+        #         "pergunta": RunnablePassthrough(),
+        #     }
+        #     # combina o contexto e a pergunta no template do prompt
+        #     | self.template_do_prompt
+        # )
 
 
         # 'StrOutputParser' é usado para tratar a saída do chat, convertendo em um string
         self.formatador_saida = StrOutputParser()
 
-        if verbose: print('--- defiindo o pipeline...')
-        self.pipeline_rag = (
-            self.base_runnable  # Usa the base runnable com os embeddings
-            | self.interface_llama  # Envia os resultados para o LLama, para geração da resposta
-            | self.formatador_saida  # Converte a saída do modelo em um formato de string
-        )
+        # if verbose: print('--- defiindo o pipeline...')
+        # self.pipeline_rag = (
+        #     self.base_runnable  # Usa the base runnable com os embeddings
+        #     | self.interface_llama  # Envia os resultados para o LLama, para geração da resposta
+        #     | self.formatador_saida  # Converte a saída do modelo em um formato de string
+        # )
 
         if verbose: print('--- inicialização completa!')
 
     def formatar_documentos_recuperados(self, docs):
             '''Função de formatação dos documentos. 'docs' é uma lista de objetos do tipo langchain_core.documents.Document'''
             return "\n\n\n\n".join([doc.page_content for doc in docs])
-    
+
     def consultar(self, pergunta, verbose=True):
         '''Recebe uma pergunta, em formato de string, realiza uma consulta no banco de vetores,
         passa os resultados para o LLM gerar uma resposta palatável e a retorna'''
 
         if verbose: print('Gerador de respostas: realizando consulta...')
-        if verbose: print(f'--- buscando texto semelhante a {pergunta}')
-        resposta = self.pipeline_rag.invoke(pergunta)
-        if verbose: print('--- consulta concluída')
-        return resposta
+        if verbose: print(f'--- Pergunta feita: {pergunta}')
+        marcador_tempo_1 = time()
+        consulta_result = self.gerenciador_de_consulta.invoke(pergunta)
+        marcador_tempo_2 = time()
+        tempo_consulta = marcador_tempo_2 - marcador_tempo_1
+        if verbose: print(f'--- consulta no banco concluída ({tempo_consulta} segundos)')
+        contexto = self.formatar_documentos_recuperados(consulta_result)
+        runnable_output = self.template_do_prompt.invoke({"pergunta": pergunta, "contexto": contexto})
+        
+        if verbose: print(f'--- gerando resposta com o Llama')
+        llama_response = self.interface_llama.invoke(runnable_output)
+        marcador_tempo_3 = time()
+        tempo_llama = marcador_tempo_3 - marcador_tempo_2
+        if verbose: print(f'--- resposta gerada em ({tempo_llama} segundos)')
+        formatted_output = self.formatador_saida.invoke(llama_response)
+
+        return {
+            'pergunta': pergunta,
+            'documentos': consulta_result,
+            'contexto': runnable_output.messages[1].content,
+            'resposta_llama': llama_response,
+            'saida': formatted_output,
+            'tempo_consulta': tempo_consulta,
+            'tempo_llama': tempo_llama
+            }
