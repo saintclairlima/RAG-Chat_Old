9aa4b399bc2c8d16e1762bd3790ba83e009f9d39
Saint-Clair <caehevanras@gmail.com>
2024-09-20 10:15:12 -0300

    Alterações para guardar contexto das perguntas anteriores

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 print('Inicializando a estrutura da API...\nImportando as bibliotecas...')
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from starlette.middleware.cors import CORSMiddleware
 from langchain_huggingface import HuggingFaceEmbeddings
-from gerador_de_respostas import GeradorDeRespostas
-from pydantic import BaseModel
+from gerador_de_respostas import GeradorDeRespostas, DadosChat
 
 print('Instanciando a api (FastAPI)')
 app = FastAPI()
@@ -19,26 +19,23 @@ print('Criando a função de embeddings')
 funcao_de_embeddings = HuggingFaceEmbeddings(
     model_name="hkunlp/instructor-xl",
     show_progress=True,
-    model_kwargs={"device": "cpu"}  # Or "cpu" if you don't have a GPU
+    model_kwargs={"device": "cpu"}
 )
 
 gerador_de_respostas = GeradorDeRespostas(funcao_de_embeddings=funcao_de_embeddings, url_vector_store='chroma_db_instructor_xl')
 
-
-# Define the Pydantic model for the POST request
-class PerguntaModel(BaseModel):
-    pergunta: str
-
 print('Definindo as rotas')
-@app.post("/get_embedding/")
-async def get_embedding(dadosRecebidos: PerguntaModel):
-    resposta = gerador_de_respostas.consultar(dadosRecebidos.pergunta)
-    return {"resposta": resposta}
 
-# # Define a GET route that takes a query parameter 'text'
-# @app.get("/get_embedding/")
-# async def get_embedding(text: str = Query(..., description="Pergunta/Query a ser buscada no banco de vetores")):
-#     resposta = gerador_de_respostas.consultar(text)
-#     return {"resposta": resposta}
+
+@app.post("/hilda/enviar_pergunta/")
+async def get_embedding(dadosRecebidos: DadosChat):
+    dados_resposta = gerador_de_respostas.consultar(dadosRecebidos)
+    return {"dados_resposta": dados_resposta}
+
+
+@app.get("/hilda/")
+async def pagina_chat():
+    with open('chat.html', 'r', encoding='utf-8') as arquivo: conteudo_html = arquivo.read()
+    return HTMLResponse(content=conteudo_html, status_code=200)
 
 print('API inicializada')
--- gerador_de_respostas.py
+++ gerador_de_respostas.py
@@ -4,8 +4,13 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+from pydantic import BaseModel
 
 from time import time
+
+class DadosChat(BaseModel):
+    pergunta: str
+    historico: list
 
 class GeradorDeRespostas:
     '''
@@ -41,8 +46,8 @@ class GeradorDeRespostas:
 
         if verbose: print('--- gerando a interface com o LLM...')
         self.interface_llama = ChatOllama(
-            model="llama3.1", # modelo llama a ser usado
-            temperature=0,  # 'temperature' controla a aleatoriedade da saída do modelo, sendo 0 o valor para saída determinística
+            model="llama3.1",    # modelo llama a ser usado
+            temperature=0,       # 'temperature' controla a aleatoriedade da saída do modelo, sendo 0 o valor para saída determinística
             base_url=url_llama,  # 'base_url' aponta para o end point da API do Llama
         )
 
@@ -51,6 +56,8 @@ class GeradorDeRespostas:
                             Use as informações do contexto fornecido para gerar uma resposta clara para a pergunta.
                             Na resposta, não mencione que foi fornecido um texto, agindo como se o contexto fornecido fosse parte do seu conhecimento próprio.
                             Quando adequado, pode citar os nomes dos documentos e números dos artigos em que a resposta se baseia.
+                            O histórico do chat deve ser revisado somente para manter o contexto, mas as perguntas do histórico não devem ser respondidas.
+                            Responda somente à pergutna atual.
                             A resposta não deve ter saudação, nem qualquer tipo de introdução que dê a entender que não houve interação anterior.
                             Assuma um tom formal, porém caloroso, com gentileza nas respostas.
                             Utilize palavras e termos que sejam claros, autoexplicativos e linguagem simples, próximo do que o cidadão comum utiliza.
@@ -62,39 +69,17 @@ class GeradorDeRespostas:
                 ('system', papel_do_LLM),  
 
                 # Placeholder para o histórico do chat manter o contexto. Durante a execução será substituído pelo histórico real do chat
-                ("placeholder", "{chat_history}"), 
+                ("system", "HISTORICO (perguntas anteriores, não devem ser respondidas, só usadas para contexto) {historico_chat}"), 
 
                 # Placeholder para o input a ser fornecido durante a execução
                 # Será substituído pela pergunta do usuário e o contexto vindo do banco de vetores
                 ("human", "CONTEXTO: {contexto} \n\nPERGUNTA: {pergunta}"),
             ]
         )
-        
-        # if verbose: print('--- gerando o runnable base e formatador de saída...')
-        # # Esse runnable faz o tratamento da pergunta e do contexto para o prompt
-        # self.base_runnable = (
-        #     {
-        #         # 'contexto' faz ouso do retrtiever/gerenciador de consulta para obter contexto do banco de vetores
-        #         # 'gerenciador_de_consulta' faz a busca no banco de vetores com base na pergunta/query
-        #         # 'formatar_documentos_recuperados' formata os resultados
-        #         "contexto": self.gerenciador_de_consulta | self.formatar_documentos_recuperados,
-        #         # 'pergunta' será passada para o retriever/gerenciador de consulta como query
-        #         "pergunta": RunnablePassthrough(),
-        #     }
-        #     # combina o contexto e a pergunta no template do prompt
-        #     | self.template_do_prompt
-        # )
 
 
         # 'StrOutputParser' é usado para tratar a saída do chat, convertendo em um string
         self.formatador_saida = StrOutputParser()
-
-        # if verbose: print('--- defiindo o pipeline...')
-        # self.pipeline_rag = (
-        #     self.base_runnable  # Usa the base runnable com os embeddings
-        #     | self.interface_llama  # Envia os resultados para o LLama, para geração da resposta
-        #     | self.formatador_saida  # Converte a saída do modelo em um formato de string
-        # )
 
         if verbose: print('--- inicialização completa!')
 
@@ -102,33 +87,40 @@ class GeradorDeRespostas:
             '''Função de formatação dos documentos. 'docs' é uma lista de objetos do tipo langchain_core.documents.Document'''
             return "\n\n\n\n".join([doc.page_content for doc in docs])
 
-    def consultar(self, pergunta, verbose=True):
+    def consultar(self, dadosChat: DadosChat, verbose=True):
         '''Recebe uma pergunta, em formato de string, realiza uma consulta no banco de vetores,
         passa os resultados para o LLM gerar uma resposta palatável e a retorna'''
 
+        pergunta = dadosChat.pergunta
+        historico_chat = [{"role": "user", "content": item} for item in dadosChat.historico]
+
         if verbose: print('Gerador de respostas: realizando consulta...')
         if verbose: print(f'--- Pergunta feita: {pergunta}')
-        marcador_tempo_1 = time()
-        consulta_result = self.gerenciador_de_consulta.invoke(pergunta)
-        marcador_tempo_2 = time()
-        tempo_consulta = marcador_tempo_2 - marcador_tempo_1
+        marcador_tempo_inicio = time()
+        documentos_retornados = self.gerenciador_de_consulta.invoke(pergunta)
+        marcador_tempo_fim = time()
+        tempo_consulta = marcador_tempo_fim - marcador_tempo_inicio
         if verbose: print(f'--- consulta no banco concluída ({tempo_consulta} segundos)')
-        contexto = self.formatar_documentos_recuperados(consulta_result)
-        runnable_output = self.template_do_prompt.invoke({"pergunta": pergunta, "contexto": contexto})
+        contexto = self.formatar_documentos_recuperados(documentos_retornados)
+        runnable_output = self.template_do_prompt.invoke({"pergunta": pergunta, "contexto": contexto, 'historico_chat': historico_chat})
         
         if verbose: print(f'--- gerando resposta com o Llama')
-        llama_response = self.interface_llama.invoke(runnable_output)
-        marcador_tempo_3 = time()
-        tempo_llama = marcador_tempo_3 - marcador_tempo_2
+        marcador_tempo_inicio = time()
+        resposta_llama = self.interface_llama.invoke(runnable_output)
+        marcador_tempo_fim = time()
+        tempo_llama = marcador_tempo_fim - marcador_tempo_inicio
         if verbose: print(f'--- resposta gerada em ({tempo_llama} segundos)')
-        formatted_output = self.formatador_saida.invoke(llama_response)
+        resposta_formatada = self.formatador_saida.invoke(resposta_llama)
+
+        dadosChat.historico.append(pergunta)
 
         return {
             'pergunta': pergunta,
-            'documentos': consulta_result,
+            'documentos': documentos_retornados,
             'contexto': runnable_output.messages[1].content,
-            'resposta_llama': llama_response,
-            'saida': formatted_output,
+            'resposta_llama': resposta_llama,
+            'resposta': resposta_formatada,
+            'historico': dadosChat.historico,
             'tempo_consulta': tempo_consulta,
             'tempo_llama': tempo_llama
             }
