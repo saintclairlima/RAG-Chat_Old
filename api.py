2406535d86ab97ed25cf6efe9c56913564ae74c3
Saint-Clair <caehevanras@gmail.com>
2024-11-29 10:39:17 -0300

    Move os arquivos referentes a interface do usuário para a pasta web

--- api.py
+++ api.py
@@ -31,31 +31,31 @@ async def gerar_resposta(dadosRecebidos: DadosChat):
 
 @app.get('/chat/')
 async def pagina_chat():
-    with open('assets/chat.html', 'r', encoding='utf-8') as arquivo: conteudo_html = arquivo.read()
+    with open('web/chat.html', 'r', encoding='utf-8') as arquivo: conteudo_html = arquivo.read()
     # substituindo as tags dentro do HTML, para maior controle
     for tag, valor in environment.TAGS_SUBSTITUICAO_HTML.items():
         conteudo_html = conteudo_html.replace(tag, valor)
     return HTMLResponse(content=conteudo_html, status_code=200)
 
-@app.get('/assets/img/favicon/favicon.ico')
-async def favicon(): return FileResponse('assets/img/favicon/favicon.ico')
+@app.get('/web/img/favicon/favicon.ico')
+async def favicon(): return FileResponse('web/img/favicon/favicon.ico')
 
-@app.get('/assets/img/favicon/favicon.svg')
-async def favicon(): return FileResponse('assets/img/favicon/favicon.svg')
+@app.get('/web/img/favicon/favicon.svg')
+async def favicon(): return FileResponse('web/img/favicon/favicon.svg')
 
-@app.get('/assets/img/favicon/favicon-48x48.png')
-async def favicon(): return FileResponse('assets/img/favicon/favicon-48x48.png')
+@app.get('/web/img/favicon/favicon-48x48.png')
+async def favicon(): return FileResponse('web/img/favicon/favicon-48x48.png')
 
-@app.get('/assets/img/favicon/apple-touch-icon.png')
-async def favicon(): return FileResponse('assets/img/favicon/apple-touch-icon.png')
+@app.get('/web/img/favicon/apple-touch-icon.png')
+async def favicon(): return FileResponse('web/img/favicon/apple-touch-icon.png')
 
-@app.get('/assets/img/favicon/site.webmanifest')
-async def favicon(): return FileResponse('assets/img/favicon/site.webmanifest')
+@app.get('/web/img/favicon/site.webmanifest')
+async def favicon(): return FileResponse('web/img/favicon/site.webmanifest')
 
-@app.get('/assets/img/Assistente.png')
-async def legisberto(): return FileResponse('assets/img/Assistente.png')
+@app.get('/web/img/Assistente.png')
+async def legisberto(): return FileResponse('web/img/Assistente.png')
 
-@app.get('/assets/img/logo_al.png')
-async def legisberto(): return FileResponse('assets/img/logo_al.png')
+@app.get('/web/img/logo_al.png')
+async def legisberto(): return FileResponse('web/img/logo_al.png')
 
 print('API inicializada')
