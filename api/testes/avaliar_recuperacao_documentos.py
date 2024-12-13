3dfef6d6b280de618f314cc49f6563484f4034db
Saint-Clair Lima <meuimeio.com>
2024-12-13 07:07:55 -0300

    Atualiza mecânica de invocaçaõ, por nomes de argumentos

--- api/testes/avaliar_recuperacao_documentos.py
+++ api/testes/avaliar_recuperacao_documentos.py
@@ -9,27 +9,32 @@ from ..utils.utils import FuncaoEmbeddings
 from time import time
 import asyncio
 import os
+import argparse
 from torch import cuda
 
 URL_LOCAL = os.path.abspath(os.path.join(os.path.dirname(__file__), "./"))
 EMBEDDING_INSTRUCTOR="hkunlp/instructor-xl"
-URL_BANCO_VETORES=os.path.join(URL_LOCAL,"../conteudo/bancos_vetores/banco_vetores_regimento_resolucoes_rh")
-NOME_COLECAO='regimento_resolucoes_rh'
 DEVICE='cuda' if cuda.is_available() else 'cpu'
 
-
-
-async def avaliar_recuperacao_documentos(url_arquivo_entrada, url_arquivo_saida=None, fazer_log=False):
-    if not url_arquivo_saida: url_arquivo_saida = url_arquivo_entrada.split('.')[0] + '_recup_docs.json'
+async def avaliar_recuperacao_documentos(
+    url_arquivo_entrada,
+    nome_banco_vetores,
+    nome_colecao,
+    url_arquivo_saida=None,
+    instrucao=None,
+    fazer_log=False):
     
-    print(f'Criando GeradorDeRespostas (usando {EMBEDDING_INSTRUCTOR})...')
-    funcao_de_embeddings = FuncaoEmbeddings(nome_modelo=EMBEDDING_INSTRUCTOR, tipo_modelo=SentenceTransformer, device=DEVICE)
-    gerador_de_respostas = GeradorDeRespostas(funcao_de_embeddings=funcao_de_embeddings, url_banco_vetores=URL_BANCO_VETORES, colecao_de_documentos=NOME_COLECAO, device=DEVICE)
+    if not url_arquivo_saida: url_arquivo_saida = url_arquivo_entrada.split('.')[0] + '_recup_docs.json'
+    url_banco_vetores = os.path.join(URL_LOCAL, f"../conteudo/bancos_vetores/{nome_banco_vetores}")
+    print(f'Criando GeradorDeRespostas (usando {EMBEDDING_INSTRUCTOR} e instrução "{instrucao}")...')
+    funcao_de_embeddings = FuncaoEmbeddings(nome_modelo=EMBEDDING_INSTRUCTOR, tipo_modelo=SentenceTransformer, device=DEVICE, instrucao=instrucao)
+    gerador_de_respostas = GeradorDeRespostas(funcao_de_embeddings=funcao_de_embeddings, url_banco_vetores=url_banco_vetores, colecao_de_documentos=nome_colecao, device=DEVICE)
 
     with open(url_arquivo_entrada, 'r') as arq:
         docs = json.load(arq)
     
-    print(f'Recuperando lista de documentos com perguntas...')
+    print(f'Recuperando lista de documentos com perguntas ({url_arquivo_entrada})...')
+    print(f'Os resultados serão salvos em {url_arquivo_saida}')
     perguntas = []
     for item in docs['dados']:
         for pergunta in item['perguntas']:
@@ -85,10 +90,24 @@ async def avaliar_recuperacao_documentos(url_arquivo_entrada, url_arquivo_saida=
 
 # Run the `avaliar` function
 if __name__ == "__main__":
-    try:
-        url_entrada = sys.argv[1]
-        url_saida = sys.argv[2]
-        asyncio.run(avaliar_recuperacao_documentos(url_arquivo_entrada=url_entrada, url_arquivo_saida=url_saida))
-    except:
-        url_entrada = sys.argv[1]
-        asyncio.run(avaliar_recuperacao_documentos(url_arquivo_entrada=url_entrada))
+    parser = argparse.ArgumentParser(description="Gera resultados de busca por documentos a partir de uma lista de perguntas")
+    
+    # Define named arguments
+    parser.add_argument('--url_entrada', type=str, required=True, help="caminho para arquivo com as perguntas")
+    parser.add_argument('--nome_banco_vetores', type=str, required=True, help="nome do banco de vetores a ser consultado")
+    parser.add_argument('--nome_colecao', type=str, required=True, help="coleçaõ do banco a ser utilizada")
+    parser.add_argument('--url_saida', type=str, help="caminho para arquivo em que serão salvos os resultados")
+    parser.add_argument('--instrucao', type=str, help="instrucao a ser utilizada na função de embeddings")
+
+    args = parser.parse_args()
+    url_entrada = args.url_entrada
+    nome_banco_vetores = args.nome_banco_vetores
+    nome_colecao = args.nome_colecao
+    url_saida = None if not args.url_saida else args.url_saida
+    instrucao = None if not args.instrucao else args.instrucao
+    asyncio.run(avaliar_recuperacao_documentos(
+        url_arquivo_entrada=url_entrada,
+        nome_banco_vetores=nome_banco_vetores,
+        nome_colecao=nome_colecao,
+        url_arquivo_saida=url_saida,
+        instrucao=instrucao))
