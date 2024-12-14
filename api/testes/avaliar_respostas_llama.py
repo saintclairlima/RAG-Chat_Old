7b12b11acc066461f2f6c26bff987146a6e104a8
Saint-Clair Lima <meuimeio.com>
2024-12-14 02:13:10 -0300

    Altera mecanismo de passagem de parâmetros para a geraçã ode respostas do llama

--- api/testes/avaliar_respostas_llama.py
+++ api/testes/avaliar_respostas_llama.py
@@ -1,5 +1,6 @@
 ## print('Para simplicidade, mover o arquivo para a pasta principal para executar')
 print('Importando bibliotecas')
+import argparse
 import json
 from time import time
 from sentence_transformers import SentenceTransformer
@@ -23,20 +24,22 @@ URL_BANCO_VETORES=os.path.join(URL_LOCAL,"../conteudo/bancos_vetores/banco_vetor
 NOME_COLECAO='regimento_resolucoes_rh'
 DEVICE='cuda' if cuda.is_available() else 'cpu'
 
-async def avaliar_respostas_llama(url_arq_entrada, url_arq_saida=None):
-    if not url_arq_saida: url_arq_saida = url_arq_entrada
+async def avaliar_respostas_llama(url_arquivo_entrada, nome_banco_vetores, nome_colecao, url_arquivo_saida=None, instrucao=None):
+    if not url_arquivo_saida: url_arquivo_saida = url_arquivo_entrada
     if FAZER_LOG: print('Carregando JSON')
-    with open(url_arq_entrada, 'r', encoding='utf-8') as arq:
+    with open(url_arquivo_entrada, 'r', encoding='utf-8') as arq:
         dados = json.load(arq)
+        dados=dados['dados'] # por motivodfe mudança na estrutura do arquivo
     if FAZER_LOG: print('Criando interface Ollama')
     interface_ollama = InterfaceOllama(url_llama=URL_LLAMA, nome_modelo=MODELO_LLAMA)
 
     if FAZER_LOG: print('Criando cliente Chroma')
-    client = chromadb.PersistentClient(path=URL_BANCO_VETORES)
+    url_banco_vetores = os.path.join(URL_LOCAL, f"../conteudo/bancos_vetores/{nome_banco_vetores}")
+    client = chromadb.PersistentClient(path=url_banco_vetores)
     if FAZER_LOG: print('Criando função de embeddings')
-    funcao_de_embeddings_sentence_tranformer = FuncaoEmbeddings(nome_modelo=EMBEDDING_INSTRUCTOR, tipo_modelo=SentenceTransformer, device=DEVICE)
+    funcao_de_embeddings_sentence_tranformer = FuncaoEmbeddings(nome_modelo=EMBEDDING_INSTRUCTOR, tipo_modelo=SentenceTransformer, instrucao=instrucao, device=DEVICE)
     if FAZER_LOG: print('Definindo Coleção')
-    collection = client.get_collection(name=NOME_COLECAO, embedding_function=funcao_de_embeddings_sentence_tranformer)
+    collection = client.get_collection(name=nome_colecao, embedding_function=funcao_de_embeddings_sentence_tranformer)
     
     if FAZER_LOG: print('Processando perguntas')
     num_itens = len(dados)
@@ -68,12 +71,37 @@ async def avaliar_respostas_llama(url_arq_entrada, url_arq_saida=None):
         item['llama'] = resp_llama
 
         if FAZER_LOG: print('salvando json')
-        with open(os.path.join(url_arq_saida), 'w', encoding='utf-8') as arq:
+        with open(os.path.join(url_arquivo_saida), 'w', encoding='utf-8') as arq:
             arq.write(json.dumps(dados, ensure_ascii=False, indent=4))
 
 if __name__ == '__main__':
-    url_json_entrada = sys.argv[1]
-    asyncio.run(avaliar_respostas_llama(url_arq_entrada=url_json_entrada))
-else:
-    url_json_entrada = sys.argv[1]
-    avaliar_respostas_llama(url_arq_entrada=url_json_entrada)
+    parser = argparse.ArgumentParser(description="Gera resultados de busca por documentos a partir de uma lista de perguntas")
+    
+    # Define named arguments
+    parser.add_argument('--url_entrada', type=str, required=True, help="caminho para arquivo com as perguntas")
+    parser.add_argument('--nome_banco_vetores', type=str, required=True, help="nome do banco de vetores a ser consultado")
+    parser.add_argument('--nome_colecao', type=str, required=True, help="coleçaõ do banco a ser utilizada")
+    parser.add_argument('--url_saida', type=str, help="caminho para arquivo em que serão salvos os resultados")
+    parser.add_argument('--instrucao', type=str, help="instrucao a ser utilizada na função de embeddings")
+
+    args = parser.parse_args()
+    url_entrada = args.url_entrada
+    nome_banco_vetores = args.nome_banco_vetores
+    nome_colecao = args.nome_colecao
+    url_saida = None if not args.url_saida else args.url_saida
+    instrucao = None if not args.instrucao else args.instrucao
+    asyncio.run(avaliar_respostas_llama(
+        url_arquivo_entrada=url_entrada,
+        nome_banco_vetores=nome_banco_vetores,
+        nome_colecao=nome_colecao,
+        url_arquivo_saida=url_saida,
+        instrucao=instrucao
+    ))
+# else:
+#     avaliar_respostas_llama(
+#         url_arquivo_entrada=url_entrada,
+#         nome_banco_vetores=nome_banco_vetores,
+#         nome_colecao=nome_colecao,
+#         url_arquivo_saida=url_saida,
+#         instrucao=instrucao
+#     )
